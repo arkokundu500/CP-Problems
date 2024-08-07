e17fb81f9fc94647fad822aa24747198883bf162
Arko Kundu <109503991+arkokundu500@users.noreply.github.com>
2024-08-08 00:37:24 +0530

    Time: 0 ms (100.00%), Space: 7.6 MB (12.41%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -1,6 +1,14 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        return n > 0 && not (n & n - 1);
+
+        
+        for(int i =0;i<31;i++){
+        int ans = pow(2,i);
+            
+            if(ans == n)return true;
+
+        }
+        return false;
     }
 };
