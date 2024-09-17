7ceff9bc38c1ecb37819a561dac7e17fc1845025
Arko Kundu <109503991+arkokundu500@users.noreply.github.com>
2024-09-17 21:59:17 +0530

    Time: 2253 ms (5.00%), Space: 58 MB (95.73%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
@@ -1,13 +1,32 @@
 class Solution {
 public:
-    int findKthLargest(vector<int>& nums, int k) {
-        int n= nums.size();
-        priority_queue<int ,vector<int>,greater<int>> pq;
-        for (int i=0;i<n;i++){
-            pq.push(nums[i]);
-            if(pq.size()>k) 
-                pq.pop();
+    int findKthLargest(std::vector<int>& nums, int k) {
+        int left = 0, right = nums.size() - 1;
+        while (true) {
+            int pivot_index = rand() % (right - left + 1) + left;
+            int new_pivot_index = partition(nums, left, right, pivot_index);
+            if (new_pivot_index == nums.size() - k) {
+                return nums[new_pivot_index];
+            } else if (new_pivot_index > nums.size() - k) {
+                right = new_pivot_index - 1;
+            } else {
+                left = new_pivot_index + 1;
+            }
         }
-        return pq.top();
+    }
+
+private:
+    int partition(std::vector<int>& nums, int left, int right, int pivot_index) {
+        int pivot = nums[pivot_index];
+        std::swap(nums[pivot_index], nums[right]);
+        int stored_index = left;
+        for (int i = left; i < right; i++) {
+            if (nums[i] < pivot) {
+                std::swap(nums[i], nums[stored_index]);
+                stored_index++;
+            }
+        }
+        std::swap(nums[right], nums[stored_index]);
+        return stored_index;
     }
 };
