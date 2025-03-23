e21c33048b6ec485a37d8b8dcedafdef1d86468a
Arko Kundu <109503991+arkokundu500@users.noreply.github.com>
2025-03-23 11:29:29 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Total Decoding Messages/total-decoding-messages.cpp
@@ -0,0 +1,45 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    int countWays(string &s) {
+        // Code here
+        int mod = 1e9 + 7, n = s.size();
+		    int prev1 = 1, prev2 = 1;
+		    for(int i = 2; i <= n; i++) {
+		        int curr = 0;
+		        if(s[i - 1] != '0') 
+		            curr = prev1;
+		        if((s[i - 2] == '1') || (s[i - 2] == '2' && s[i - 1] <= '6'))
+		            curr = (curr + prev2) % mod;
+		        prev2 = prev1;
+		        prev1 = curr;
+		    }
+		    return prev1;
+    }
+};
+
+
+//{ Driver Code Starts.
+int main() {
+    int tc;
+    cin >> tc;
+    cin.ignore();
+    while (tc--) {
+        string digits;
+        getline(cin, digits);
+        Solution obj;
+        int ans = obj.countWays(digits);
+        cout << ans << "\n";
+
+        cout << "~"
+             << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
